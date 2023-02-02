62e34bee1e3d141c7e1108bb391cb37d8561ed06
HyunSeok Hong <hsuk@kakao.com>
2023-02-02 15:17:01 +0900

    Update util.py
    added verbose options for functions

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 # UTILITIES
 import os, io
 
-def read_files(dir, ftype="txt"):
+def read_files(dir, ftype="txt", verbose=True):
     import fitz
 
     prev_dir = os.getcwd()
@@ -11,18 +11,17 @@ def read_files(dir, ftype="txt"):
     file_list = [ file_name for file_name in os.listdir(dir) 
                     if file_name[-len(ftype):] == ftype ]
     for file_name in file_list:
-        #TODO : remove slicing after devel
-        print(f"reading {file_name}")
+        if verbose: print(f"reading {file_name}")
         yield fitz.open(os.path.join(file_name))
     os.chdir(prev_dir)
 
-def read_images_from_pdf(files, out_dir):
+def read_images_from_pdf(files, out_dir, verbose=True):
     from PIL import Image
 
     for pdf in files:
         file_name = os.path.splitext(pdf.name)[0]
         images = sum([ pdf[i].get_images() for i in range(len(pdf)) ], [])
-        print(f"{len(images)} images in file : {file_name}")
+        if verbose: print(f"{len(images)} images in file : {file_name}")
 
         page_out_dir = os.path.join(out_dir, file_name)
         os.makedirs(page_out_dir)
@@ -33,7 +32,7 @@ def read_images_from_pdf(files, out_dir):
             image = Image.open(io.BytesIO(extract["image"]))
             image.save(open(f"{page_out_dir}/img_{n}.{extract['ext']}", "wb"))
 
-def read_text_from_pdf(files, out_dir):
+def read_text_from_pdf(files, out_dir, verbose=True):
     for pdf in files:
 
         file_name = os.path.splitext(pdf.name)[0]
@@ -41,7 +40,7 @@ def read_text_from_pdf(files, out_dir):
         text = str()
 
         for page in pdf: text += page.getText()
-        print(text)
+        if verbose: print(text)
 
         with open(f"{out_file}.txt", "w") as f:
             f.write(text)
